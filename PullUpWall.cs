152eba91a5b613f5b0f58763cfdddd65f14ac4a7
Pythagean <jeremysto@gmail.com>
2017-01-22 15:43:25 +1300

    Initial Commit

---
+++ PullUpWall.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullUpWall : MonoBehaviour {
+
+
+	public AudioClip rockSound;
+	public GameObject wall;
+	private AudioSource source;
+	public OVRInput.Controller Controller;
+
+	private bool playedWallSound = false;
+
+	void Awake() {
+		source = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		if (OVRInput.Axis1D.PrimaryHandTrigger.ToString() == 1){
+//			Debug.Log (Time.time);
+//		}
+
+		//Debug.Log (Input.GetAxis ("RHandTrigger").ToString ());
+		//Debug.Log (OVRInput.Axis1D.PrimaryHandTrigger.ToString());
+		//ebug.Log (OVRInput.GetActiveController ().ToString ());
+		if (Input.GetAxis("RHandTrigger") == 1 || Input.GetAxis("LHandTrigger") == 1) {
+			//Debug.Log ("Right");
+			//createRockWall();
+			transform.localPosition = OVRInput.GetLocalControllerPosition(Controller);
+
+			playSound();
+
+		}
+
+
+	
+	}
+
+	void playSound(){
+		if (!playedWallSound) {
+			source.PlayOneShot(rockSound, 1f);
+			playedWallSound = true;
+		}
+	}
+}
---
+++ TouchController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchController : MonoBehaviour {
+
+	public AudioClip rockSound;
+	public GameObject wall;
+	public OVRInput.Controller Controller;
+	private AudioSource source;
+	public GameObject Player;
+
+	private Transform toLane;
+
+	private int wallCooldownCounterMax = 1000;
+	private int wallCooldownCounter = 1000;
+	private bool wallCooldown = false;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = OVRInput.GetLocalControllerPosition(Controller);
+		transform.localRotation = OVRInput.GetLocalControllerRotation(Controller);
+
+		//Debug.Log (wallCooldownCounter.ToString ());
+
+
+		if (Input.GetAxis("RHandTrigger") == 1 || Input.GetAxis("LHandTrigger") == 1) {
+			//Debug.Log ("Right");
+			if (!wallCooldown){
+				Debug.Log (transform.position.ToString ());
+
+				Vector3 direction = (toLane.transform.position - transform.position).normalized;
+				//Vector3 distance = Vector3.Distance(
+
+				GameObject createdWall = Instantiate(wall, transform.position, Quaternion.identity) as GameObject;
+				wallCooldown = true;
+			}
+
+
+
+
+		}
+
+		if (wallCooldown) {
+			wallCooldownCounter--;
+			if (wallCooldownCounter < 0) {
+				wallCooldownCounter = wallCooldownCounterMax;
+				wallCooldown = false;
+			}
+		}
+
+
+		if (Input.GetButtonDown("Fire1")) {
+		//	source.PlayOneShot (rockSound, 1.0f);
+		}
+		//if (OVRInput.GetDown(OVRInput.Axis1D.PrimaryHandTrigger)) {
+		//}
+
+		//if (Input.GetButtonDown (OVRInput.Axis1D.PrimaryHandTrigger)) {
+		//}
+	}
+
+
+
+
+}
